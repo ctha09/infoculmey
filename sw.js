a8835b006f7ce97ca9a594bc7347447a591750dc
CDE <carlostradingfxft@gmail.com>
2026-03-30 00:00:38 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-self.addEventListener('fetch', () => {}); // Mínimo para que sea instalable
+const CACHE_NAME = 'infoculmey-cache-v1';
+const urlsToCache = ['./', './index.html', './style.css', './script.js', './icon-192.png'];
+
+// Instalación y almacenamiento en caché
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Estrategia de respuesta: Primero red, si falla, caché
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
