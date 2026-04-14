8f3dc2385c2746e47961e763b5fefac8ce4db9d0
CDE <carlostradingfxft@gmail.com>
2026-04-14 20:34:56 -0300

    Add new sales and purchase entries to script.js

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const datosInicialesTesoreria = [
     { desc: "Compra de pilas para nuevas calculadoras", amount: -48452 },
     { desc: "Venta de pizzetas", amount: 15000 },
     { desc: "Venta de kasestanguen", amount: 27000 }
+    { desc: "Venta de pizza", amount: 1600 }
+    { desc: "Venta de pizza", amount: 15100 }
+    { desc: "Compra de fotocopias", amount: -2500 }
 ];
 
 const datosInicialesPrensa = [
